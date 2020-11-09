8392e5ef08da628208f197e7f8810fe378a03242
Paweł <lesniakcbr@gmail.com>
2020-11-09 15:49:54 +0100

    Added dependencies and Main function. Imported Selenium WebDriver and ChromeDriver.

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+public class Main {
+
+    public static void main(String [] args)
+    {
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://3.209.82.53/");
+    }
+
+    //https://www.selenium.dev/documentation/en/webdriver/browser_manipulation/
+}
